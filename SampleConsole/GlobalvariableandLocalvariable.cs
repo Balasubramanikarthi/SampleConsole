8edfd0898d506ee7010dc5518ca6d12a951b0609
KarthiBalasubramani <karthikeyan.selvisubramani@gmail.com>
2025-08-08 13:08:02 +0530

    This is Yesterday Work .

---
+++ SampleConsole/GlobalvariableandLocalvariable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsole
+{
+    class GlobalvariableandLocalvariable
+    {
+        string _name = "Money";
+        int _money = 1000;
+        public void Method1()
+        {
+            string name = "Amount";
+            int amount = 100;
+
+        }
+        public void  Method2()
+        {
+            Console.WriteLine(_name);
+            string name = "Savings";
+        }
+    }
+}
